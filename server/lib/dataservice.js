f447eaade6afa1694eab9e55e4c22048545794b5
Seya Obey <seyaobey@gmail.com>
2016-10-01 01:55:12 +0300

    22

--- server/lib/dataservice.js
+++ server/lib/dataservice.js
@@ -1,16 +1,19 @@
 "use strict";
 var root = require('root-path');
-var br_sequel = require(root('/server/breeze_sequel/main'));
-var sequel_manager = br_sequel.SequelizeManager;
-var sequel_query = br_sequel.SequelizeQuery;
-var sequel_save = br_sequel.SequelizeSaveHandler;
-var __sequel = require('sequelize');
-require('./adapter');
-br_sequel.breeze.config.initializeAdapterInstance('dataService', 'adapter_webApi', true);
-var DataService = (function () {
-    function DataService() {
-    }
-    return DataService;
-}());
+////import ctx = require('../appcontext');
+//import Q = require('q');
+//var br_sequel = require(root('/server/breeze_sequel/main'));
+//var sequel_manager = br_sequel.SequelizeManager;
+//var sequel_query = br_sequel.SequelizeQuery;
+//var sequel_save = br_sequel.SequelizeSaveHandler;
+//var __sequel = require('sequelize');
+////import utils = require('./utils');
+//import _ = require('lodash');
+////import Store = require('./store');
+//import breeze = require('breeze-client');
+//require('./adapter');
+//br_sequel.breeze.config.initializeAdapterInstance('dataService', 'adapter_webApi', true);
+class DataService {
+}
 exports.DataService = DataService;
 //# sourceMappingURL=dataservice.js.map
--- server/lib/dataservice.ts
+++ server/lib/dataservice.ts
@@ -1,21 +1,21 @@
 ﻿
 var root = require('root-path');
 
-//import ctx = require('../appcontext');
-import Q = require('q');
-var br_sequel = require(root('/server/breeze_sequel/main'));
-var sequel_manager = br_sequel.SequelizeManager;
-var sequel_query = br_sequel.SequelizeQuery;
-var sequel_save = br_sequel.SequelizeSaveHandler;
-var __sequel = require('sequelize');
-//import utils = require('./utils');
-import _ = require('lodash');
-//import Store = require('./store');
-import breeze = require('breeze-client');
-require('./adapter');
+////import ctx = require('../appcontext');
+//import Q = require('q');
+//var br_sequel = require(root('/server/breeze_sequel/main'));
+//var sequel_manager = br_sequel.SequelizeManager;
+//var sequel_query = br_sequel.SequelizeQuery;
+//var sequel_save = br_sequel.SequelizeSaveHandler;
+//var __sequel = require('sequelize');
+////import utils = require('./utils');
+//import _ = require('lodash');
+////import Store = require('./store');
+//import breeze = require('breeze-client');
+//require('./adapter');
 
 
-br_sequel.breeze.config.initializeAdapterInstance('dataService', 'adapter_webApi', true);
+//br_sequel.breeze.config.initializeAdapterInstance('dataService', 'adapter_webApi', true);
 
 
 
