c0f7830a5516e68454873f04e93ea00bb90809fc
Seya Obey <seyaobey@gmail.com>
2016-10-03 17:19:37 +0300

    port 1337

--- server/dispatcher.js
+++ server/dispatcher.js
@@ -1,9 +1,59 @@
 /// <reference path="lib/dataservice.ts" />
 /// <reference path="lib/appcontext.ts" />
+/// <reference path="lib/dataservice.ts" />
+/// <reference path="datastore/store.ts" />
 "use strict";
 var ds = require('./lib/dataservice');
 var ctx = require('./lib/appcontext');
 var breeze = require('breeze-client');
+var dal = require('./lib/dataservice');
+var store = require('./datastore/store');
+function sendResponse(data, res) {
+    res.send(data);
+}
+exports.operationtype = {
+    fetch: 'fetch',
+    metadata: 'metadata',
+};
+function dispatch_call(operation, req, res, next) {
+    switch (operation) {
+        case exports.operationtype.metadata:
+            {
+                fetch_metadata(req, res, next);
+            }
+            break;
+        case exports.operationtype.fetch:
+            {
+                fetch_data(req, res, next);
+            }
+            break;
+    }
+}
+exports.dispatch_call = dispatch_call;
+function format_qry(qry) {
+    var str_qry = JSON.stringify(qry);
+    return JSON.parse(str_qry, function (key, val) {
+        if (val === '___NULL___') {
+            return null;
+        }
+        return val;
+    });
+}
+function fetch_data(req, res, next) {
+    var __qry = format_qry(req.body);
+    var qry = new breeze.EntityQuery(__qry);
+    var _ctx = new ctx.AppContext();
+    var srv = new dal.DataService(_ctx, qry.resourceName);
+    srv.fetch(qry).then(function (rst) {
+        var response = {
+            payload: srv.datasource.exportEntities()
+        };
+        res.send(response);
+    });
+}
+function fetch_metadata(req, res, next) {
+    res.send(store.ModelStore.exportMetadata());
+}
 function test(req, res) {
     var app_ctx = new ctx.AppContext();
     var s = new ds.DataService(app_ctx, 'item');
