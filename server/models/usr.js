77a6f12698c3c32def4df55523642636d106d2a9
Seya Obey <seyaobey@gmail.com>
2016-11-19 19:04:12 +0200

    usar/usrs

--- server/models/usr.js
+++ server/models/usr.js
@@ -11,6 +11,48 @@ store.add_to_Store({
         usremail: { dataType: breeze.DataType.String },
         usrpic: { dataType: breeze.DataType.String },
         usrstatus: { dataType: breeze.DataType.Int32 },
+    },
+    navigationProperties: {
+        usra: {
+            entityTypeName: "usra",
+            associationName: "rel_usr_usra",
+            isScalar: false
+        },
+        usrs: {
+            entityTypeName: "usrs",
+            associationName: "rel_usr_usrs",
+            isScalar: false
+        }
+    }
+});
+store.add_to_Store({
+    defaultResourceName: 'usra',
+    dataProperties: {
+        id: { dataType: breeze.DataType.String, isPartOfKey: true },
+        usrid: { dataType: breeze.DataType.String },
+        actid: { dataType: breeze.DataType.String }
+    },
+    navigationProperties: {
+        item: {
+            type: "usr",
+            assoc: "rel_usr_usra",
+            foreignKeyNames: ["usrid"]
+        }
+    }
+});
+store.add_to_Store({
+    defaultResourceName: 'usrs',
+    dataProperties: {
+        id: { dataType: breeze.DataType.String, isPartOfKey: true },
+        usrid: { dataType: breeze.DataType.String },
+        sklsid: { dataType: breeze.DataType.String }
+    },
+    navigationProperties: {
+        item: {
+            type: "usr",
+            assoc: "rel_usr_usrs",
+            foreignKeyNames: ["usrid"]
+        }
     }
 });
 //# sourceMappingURL=usr.js.map
