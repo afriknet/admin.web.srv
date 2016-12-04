4422b7c602b79ed079e434e8da9ce63b82b72c33
Seya Obey <seyaobey@gmail.com>
2016-12-04 10:26:21 +0200

    usr_jbr

--- server/models/usr.ts
+++ server/models/usr.ts
@@ -65,3 +65,28 @@ store.add_to_Store({
             }
     }
 });
+
+
+store.add_to_Store({
+    defaultResourceName: 'jbr',
+    dataProperties: {
+        id: { dataType: breeze.DataType.String, isPartOfKey: true },
+        compid: { dataType: breeze.DataType.String },
+        empid: { dataType: breeze.DataType.String },
+        deptid: { dataType: breeze.DataType.String },
+        jobdescr: { dataType: breeze.DataType.String },
+        jbrcomp: { dataType: breeze.DataType.String },
+        jbrlocation: { dataType: breeze.DataType.String },
+        jobstartdate: { dataType: breeze.DataType.DateTime },
+        jobenddate: { dataType: breeze.DataType.DateTime },
+        jobnotes: { dataType: breeze.DataType.String }
+    },
+
+    navigationProperties: {
+        emp: {
+            type: "usr",
+            assoc: "rel_usr_jbr",
+            foreignKeyNames: ["usrid"]
+        }
+    }
+});
--- server/services/usr.ts
+++ server/services/usr.ts
@@ -2,11 +2,12 @@
 /// <reference path="../lib/dataservice.ts" />
 
 import srv = require('../lib/dataservice');
+import Q = require('q');
+var guid = require('guid');
+import breeze = require('breeze-client');
 
 
 
 export class UsrSrv extends srv.DataService {
-
-
-
+    
 }
